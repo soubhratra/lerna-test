547a7c154adf4276ae1ad306ea3210a85046366f
soubhratra <soubhratra@gmail.com>
2020-07-03 14:46:49 -0400

    initial revision

---
+++ packages/my-module/lib/HelloWorld.js
@@ -0,0 +1,3 @@
+import * as React from "react";
+export var HelloWorld = function () { return React.createElement("div", null, "HelloWorld again"); };
+//# sourceMappingURL=HelloWorld.js.map
---
+++ packages/my-module/lib/Helloworld.js
@@ -0,0 +1,3 @@
+import * as React from "react";
+export var HelloWorld = function () { return React.createElement("div", null, "HelloWorld"); };
+//# sourceMappingURL=Helloworld.js.map
